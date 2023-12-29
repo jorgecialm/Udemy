2d5121cc3ad075bed4c4af11590f40bf7b2f4b12
jorgecialm <jorgecialm@gmail.com>
2023-12-29 11:01:02 -0300

    3

---
+++ Python/Dia4/Operadores_De_Comparacion.py
@@ -0,0 +1,4 @@
+mi_bool= 10==25
+print(mi_bool)
+mi_bool1='blanco'=='Blanco'.lower()
+print(mi_bool1)
---
+++ Python/Dia4/Operadores_Logicos.py
@@ -0,0 +1,4 @@
+mi_bool= 4<5 and 5>6
+print(mi_bool)
+mi_bool1= 10 == 10 or 3==3
+print(mi_bool1)
